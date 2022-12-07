76c1b786c4486e588f4522f139b471fdaba3fe90
RollMocha <102024018+RollMocha@users.noreply.github.com>
2022-12-07 16:38:49 +0900

    init

--- app.js
+++ app.js
@@ -1,41 +1,205 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const http = require("http");
+const mysql = require("mysql");
+const session = require('express-session');
+const bodyParser = require("body-parser");
+const PORT = 8001;
+const ejs = require('ejs');
+const cookie = require('cookie-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let sign_cnt = 0;
+let add_cnt = 0;
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const db = mysql.createConnection({
+  user:'root',
+  password:'1111',
+  database:'cafe24'
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(bodyParser.urlencoded({extended: false}));
+// app.use(session({
+//   secret: 'my key',	// 원하는 문자 입력
+//   resave: true,
+//   saveUninitialized: true,
+// }))
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.set(express.static("views"));
+app.set('view engine', 'ejs');
+
+
+app.get("/", function(req, res) {
+  fs.readFile('./routes/index.html', "utf-8", function(err, data) {
+    res.send(data);
+  });
+});
+app.post("/", function(req, res) {
+    const{userID, userPassword} = req.body;
+    console.log(userID);
+    db.query("select * from member where userID = ? and userPassword = ?",[userID, userPassword], function(err,result){
+      if (err) {
+       console.log(err);
+      }
+      else if(result.length > 0){
+        console.log("로그인 성공");
+        res.redirect("/board");
+      }
+      else {
+        res.send("<script>alert('로그인 실패'); window.history.back();</script>");
+      }
+    });
+});
+app.get("/sign_up", function(req, res) {
+  fs.readFile('./routes/sign_up.html', "utf-8", function(err, data) {
+    res.send(data);
+  });
 });
 
-module.exports = app;
+app.post("/sign_up", function(req, res) {
+    const {id, password, name, email} = req.body;
+    db.query("insert into member(userID, userPassword, name, email) values (?, ?, ?, ?)", [id, password, name, email], function(err, result) {
+      if (err) {
+          console.log(err);
+          return;
+      } 
+      else {
+          res.redirect("/sign_up");
+      }
+  });
+});
+app.get("/board", function(req, res) {
+  fs.readFile('./routes/board.html', "utf-8", function(err, data) {
+    db.query('select * from board', function(error, result){
+      res.send(ejs.render(data, {result:result}));
+    });
+  });
+});
+app.post("/board", function(req, res) {
+  const {box, search} = req.body;
+  console.log(search);
+  fs.readFile('./routes/board.html', "utf-8", function(err, data){
+    if(box == 2){
+      db.query('select * from board where board_no = ?',[search], function(err, result){
+        if(err){
+          console.log(err);
+        }
+        else{
+          res.send(ejs.render(data, {result:result}));
+        }
+      });
+    }
+    else if(box == 1){
+      db.query('select * from board where board_title = ?',[search], function(err, result){
+        if(err){
+          console.log(err);
+        }
+        else{
+          res.send(ejs.render(data, {result:result}));
+        }
+      });
+    }
+    else if(box == 0){
+      db.query('select * from board', function(err, result){
+        if(err){
+          console.log(err);
+        }
+        else{
+          res.send(ejs.render(data, {result:result}));
+        }
+      });
+    }
+    else{
+      console.log("문제있음");
+    }
+  });
+});
+
+app.get("/add", function(req, res) {
+  fs.readFile('./routes/add.html', "utf-8", function(err, data) {
+    res.send(data);
+  });
+});
+
+app.post("/add", function(req, res) {
+  const {title, content, password} = req.body;
+  db.query("insert into cafe24.board(board_title, board_content, board_password) values (?, ?, ?)", [title, content, password], function(err, result) {
+    if (err) {
+        console.log(err);
+        return;
+    } 
+    else {
+        res.redirect("/board");
+    }
+  });
+});
+
+app.get("/space/:id", function(req, res) {
+  fs.readFile("./routes/space.html", "utf8", function(err, data) {
+      db.query("select * from board where board_no = ?", [req.params.id], function(err, result) {
+        if(err){
+          console.log(err);
+        }
+        else{
+          res.send(ejs.render(data, {result:result[0]}));
+        }
+      });
+  });
+});
+app.post("/space/:id", function(req, res) {
+  const {id,title, content, password} = req.body;
+  db.query("select * from cafe24.board where board_password = ?",[password],function(err,result){
+    if(err){
+      console.log(err);
+    }
+    else if(result.length > 0){
+      db.query("update cafe24.board set board_title = ?, board_content = ? where board_no = ?", [title, content, id], function(err) {
+        if(err){
+          console.log(err);
+        }
+        else{
+          res.redirect("/board");
+        }
+      });
+    }
+    else{
+      res.send("<script>alert('비밀번호가 틀립니다.'); window.history.back();</script>");
+    }
+  })
+});
+app.get("/delete/:id", function(req, res){
+  fs.readFile('./routes/delete.html', "utf8", function(err, data) {
+    db.query("select * from board where board_no = ?", [req.params.id], function(err, result) {
+      if(err){
+        console.log(err);
+      }
+      else{
+        res.send(ejs.render(data, {result:result[0]}));
+      }
+    });
+  });
+});
+app.post("/delete/:id", function(req, res){
+  const {id, password} = req.body;
+  console.log(id);
+  console.log(password);
+  db.query("select * from cafe24.board where board_password = ?",[password],function(err,result){
+    if(err){
+      console.log(err);
+    }
+    else if(result.length > 0){
+      console.log(result.length);
+      db.query("delete from cafe24.board where board_no = ?", [id], function(err) {
+        if(!err){
+            res.redirect("/board");
+        }
+      });
+    }
+    else{
+      res.send("<script>alert('비밀번호가 틀립니다.'); window.history.back();</script>");
+    }
+  });
+});
+app.listen(PORT, () => {
+    console.log(`server started on PORT ${PORT}`);
+});
